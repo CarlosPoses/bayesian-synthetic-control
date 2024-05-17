1922552461d1bfebda1e31dac6b72b7a0d0be997
CarlosPoses <carlosgonzalezposes@gmail.com>
2024-05-17 15:26:19 +0200

    Update 01.load-data.R
    adjust directory

--- code/analysis-and-figures/01.load-data.R
+++ code/analysis-and-figures/01.load-data.R
@@ -1,11 +1,11 @@
 # Read data
-weights <- readRDS("complete_simulation/processed_results/weights.rds")
-effects <- readRDS("complete_simulation/processed_results/effects.rds")
-conditions <- readRDS("complete_simulation/processed_results/conditions.rds")
-covariates <- readRDS("complete_simulation/processed_results/covariates.rds")
-mess_and_diag <- readRDS("complete_simulation/processed_results/mess_and_diag.rds")
-pretreatment_outcomes <- readRDS("complete_simulation/processed_results/pretreatment_outcomes.rds")
-posible_synt <- readRDS("complete_simulation/processed_results/possible_synt.rds")
+weights <- readRDS("sim-output/processed_results/weights.rds")
+effects <- readRDS("sim-output/processed_results/effects.rds")
+conditions <- readRDS("sim-output/processed_results/conditions.rds")
+covariates <- readRDS("sim-output/processed_results/covariates.rds")
+mess_and_diag <- readRDS("sim-output/processed_results/mess_and_diag.rds")
+pretreatment_outcomes <- readRDS("sim-output/processed_results/pretreatment_outcomes.rds")
+posible_synt <- readRDS("sim-output/processed_results/possible_synt.rds")
 
 
 
